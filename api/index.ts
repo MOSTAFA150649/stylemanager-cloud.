36a1e1ddfc27b8401214420d25fbe107e93cd19c
Talbi <talbi@stylemanager.com>
2026-03-28 23:46:47 +0100

    Instrument api/index.ts to catch Vercel 500 error

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,18 @@
-import app from '../backend/src/server';
+let app;
+try {
+  // on essaie de charger l'application
+  app = require('../backend/src/server').default;
+} catch (error: any) {
+  // en cas de crash, on crée un faux serveur qui renvoie l'erreur en clair !
+  console.error("Crash au démarrage de Vercel :", error);
+  app = require('express')();
+  app.use((req: any, res: any) => {
+    res.status(500).json({
+      titre: 'CRASH VERCEL (FUNCTION_INVOCATION_FAILED)',
+      message: error.message,
+      stack: error.stack,
+    });
+  });
+}
+
 export default app;
